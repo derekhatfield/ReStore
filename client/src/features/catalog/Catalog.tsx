9c0be8e048d225f919ce50a689094277db2f4963
Derek <dhatfield2548@gmail.com>
2023-01-17 17:26:17 -0500

    End of section 6

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Product } from "../../app/layout/models/product"
+import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
 
 export default function Catalog() {
