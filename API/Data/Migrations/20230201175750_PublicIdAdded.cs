ed8256281407a86e93f3df5da0e6241409715619
Derek <dhatfield2548@gmail.com>
2023-02-01 16:44:53 -0500

    End of section 13

---
+++ API/Data/Migrations/20230201175750_PublicIdAdded.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class PublicIdAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Products");
+        }
+    }
+}
